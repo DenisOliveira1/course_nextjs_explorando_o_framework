0bf29de2fa834f63bbe1ee78afc633e2e24813b6
Denis Oliveira <doliveira@vidalink.com.br>
2023-11-08 13:35:12 -0300

    Finalizando unidade 1 implementando SPA com nextjs

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Link from '../src/components/Link'
+
+export default function Page404() {
+    return (
+        <div>
+            <h1>404 - Não Encontrado</h1>
+            <Link href="/">
+                Ir para a Home
+            </Link>
+        </div>
+    )
+}
