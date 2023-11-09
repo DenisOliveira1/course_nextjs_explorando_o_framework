fb4fdc37b3912065d3230aa9772c4bdb456c799f
Denis Oliveira <doliveira@vidalink.com.br>
2023-11-08 21:05:25 -0300

    Finalizando unidade 3

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
-import Link from '../src/components/Link'
-import Title from '../src/components/Title'
+import HomeScreen from "../src/screens/HomeScreen";
 
-export default function HomePage() {
-    return (
-        <div>
-            <Title as="h1">Alura Cases - Home</Title>
-            <Link href="/faq">
-                Ir para o FAQ
-            </Link>
-        </div>
-    )
-}
+export default HomeScreen;
+
+// import Footer from '../src/components/patterns/Footer'
+// import Link from '../src/components/Link'
+// import Title from '../src/components/Title'
+
+// export default function HomePage() {
+//     return (
+//         <div>
+//             <Title as="h1">Alura Cases - Home</Title>
+//             <Link href="/faq">
+//                 Ir para o FAQ
+//             </Link>
+//             <Footer/>
+//         </div>
+//     )
+// }
