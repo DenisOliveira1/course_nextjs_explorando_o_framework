ba44d2cdf47c8699686b3b1f4d043452947cc83f
Denis Oliveira <doliveira@vidalink.com.br>
2023-11-16 11:26:41 -0300

    Adicionando observacao para getStaticProps

--- pages/faq.js
+++ pages/faq.js
@@ -1,5 +1,9 @@
 import Link from '../src/components/Link'
 
+// o getStaticProps roda uma vez, pega as informaçoes do GET e armazena antes do start no yarn dev ou
+// antes do yarn export, que faz o build, logo se o build for reexecutado, mesmo que o retorno do GET mude,
+// ele sera o mesmo do momento do build.
+// ou seja, mesmo se o site do GET cair, o nosso serviço vai apresentar o conteudo que pass a ser de forma estatica
 export async function getStaticProps() {
     const FAQ_API_URL = "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
 
